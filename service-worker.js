824332a0c59984fd7689d1cdb8efc2019cd0519e
larsyul <larsyul@gmail.com>
2026-08-25 17:02:47 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,48 @@
-const CACHE_NAME = 'hoya-pwa-v3';
+const CACHE_NAME = 'hoya-pwa-v5';
+
 const APP_SHELL = [
   './',
   './index.html',
-  './manifest.webmanifest?v=2',
-  './icon-192.png?v=2',
-  './icon-512.png?v=2'
+  './manifest.webmanifest?v=4',
+  './icon-192.png?v=4'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
+
   if (url.origin !== self.location.origin) return;
+
   event.respondWith(
-    fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-      return response;
-    }).catch(() => caches.match(event.request))
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => cache.put(event.request, copy));
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
