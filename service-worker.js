fea1e883d0a37387191daa5515ea26b78ace100b
larsyul <larsyul@gmail.com>
2026-09-16 09:56:59 +0900

    Refresh Hoya cache metadata

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'hoya-pwa-v5';
+const CACHE_NAME = 'hoya-pwa-v6';
 
 const APP_SHELL = [
   './',
   './index.html',
-  './manifest.webmanifest?v=4',
-  './icon-192.png?v=4'
+  './manifest.webmanifest?v=6',
+  './icon-192.png?v=6'
 ];
 
 self.addEventListener('install', event => {
